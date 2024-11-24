ada815c46f22203e04cbf8ae36a8875bb9b0afd9
jayanegaras <jayanegara241@gmail.com>
2024-11-24 17:47:17 +0800

    ScrollTop
    Scroll To Top Button

--- scrollTop.js
+++ scrollTop.js
@@ -1,4 +1,10 @@
 document.addEventListener("DOMContentLoaded", ()=>{
+    const scrollTop = document.getElementById('scrollTop');
+    scrollTop.addEventListener('click', ()=>{
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'  // Smooth scrolling effect
+          });
+    });
 
-    
 });
